cf1412debb27db05462591633d8493105af9cc78
davidmichaelryan <davidryan2015@u.northwestern.edu>
2014-08-30 20:59:31 -0500

    start of something neeewwww

---
+++ js/global.js
@@ -0,0 +1,8 @@
+$(window).resize(function(){
+  var circleImages = $('.circle-img img');
+  circleImages.height(circleImages.width());
+});
+$(document).ready(function(){
+  var circleImages = $('.circle-img img');
+  circleImages.height(circleImages.width());
+});
