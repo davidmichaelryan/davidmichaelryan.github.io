d19b7e0ee82d2a2cf168c8aab9fedb9fd3c4b018
davidmichaelryan <davidryan2015@u.northwestern.edu>
2014-12-20 20:46:37 -0500

    benefit:

--- js/global.js
+++ js/global.js
@@ -1,8 +1,13 @@
+$(window).load(function(){
+  var circleImages = $('.circle-img img');
+  circleImages.height(circleImages.width());
+  pop_in(circleImages);
+});
+
 $(window).resize(function(){
   var circleImages = $('.circle-img img');
   circleImages.height(circleImages.width());
 });
-$(document).ready(function(){
-  var circleImages = $('.circle-img img');
-  circleImages.height(circleImages.width());
-});
+
+function pop_in(obj){
+}
