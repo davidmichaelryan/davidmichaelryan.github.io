5a1efefb569cdda90bb2316dd783f615c9b97864
David Ryan <davidmichaelryan1@gmail.com>
2015-09-26 03:08:16 -0500

    update 1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,6 +55,6 @@ module.exports = function(grunt) {
 
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['stylus', 'concat', 'uglify', 'imagemin', 'watch']);
+    grunt.registerTask('default', ['stylus', 'concat', 'uglify', 'watch']);
 
 };
